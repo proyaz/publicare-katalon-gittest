c1309c12fecd35ef4824e8bd1b5e41299e56bbf8
Nasrullah <nasrullah.syed@silbury.com>
2019-03-20 18:15:22 +0530

    added test suites

---
+++ Test Suites/Publicare-TestSuite.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Publicare-TestSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>c2d8cb67-c0a5-4a68-8fd3-75b9a51f564f</testSuiteGuid>
+   <testCaseLink>
+      <guid>5c4325be-22a8-4aac-a3a1-a008b31c933e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Publicare Services</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>804319a2-64e0-4817-9af0-e1d08c82b660</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Publicare Products</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
