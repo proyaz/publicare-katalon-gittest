60c4d4d55679a19abe38a70d775c679d074de984
Nasrullah <nasrullah.syed@silbury.com>
2019-03-25 19:14:04 +0530

    added .settings, Reports folder, bin, run_chrome .sh files and modified .gitignore file

---
+++ run_chrome.sh
@@ -0,0 +1,6 @@
+
+#!/usr/bin/env bash
+
+set -xe
+
+docker run -t --rm -v "$(pwd)":/katalon/katalon/source katalonstudio/katalon katalon-execute.sh -browserType="Chrome" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/Publicare-TestSuite"
