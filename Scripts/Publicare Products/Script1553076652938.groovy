d1d6927f86bd2c08b39100866ba05ee33de323ee
Nasrullah <nasrullah.syed@silbury.com>
2019-03-26 16:36:39 +0530

    reverted the code with no explicit driver settings

--- Scripts/Publicare Products/Script1553076652938.groovy
+++ Scripts/Publicare Products/Script1553076652938.groovy
@@ -13,30 +13,7 @@ import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
 
-// Explicitly set the path of Chrome Driver binary
-System.setProperty("webdriver.chrome.driver", "/var/jenkins_home/tools/chromedriver/chromedriver");
-
-// Explicitly set the path of Chrome Browser binary
-import org.openqa.selenium.chrome.ChromeDriver
-import org.openqa.selenium.chrome.ChromeOptions
-import org.openqa.selenium.remote.DesiredCapabilities
-import com.kms.katalon.core.webui.driver.DriverFactory
-
-optionsBeta = new ChromeOptions();
-//optionsBeta.addArguments("start-maximized"); // open Browser in maximized mode
-//optionsBeta.addArguments("disable-infobars"); // disabling infobars
-//optionsBeta.addArguments("--disable-extensions"); // disabling extensions
-//optionsBeta.addArguments("--disable-gpu"); // applicable to windows os only
-//optionsBeta.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-optionsBeta.addArguments("--no-sandbox"); // Bypass OS security model
-optionsBeta.addArguments('--headless');
-optionsBeta.setBinary("/var/jenkins_home/tools/chromedriver/chromedriver");
-DesiredCapabilities capabilities = new DesiredCapabilities();
-capabilities.setCapability(ChromeOptions.CAPABILITY, optionsBeta);
-
-ChromeDriver driver = new ChromeDriver(capabilities);
-DriverFactory.changeWebDriver(driver)
-
+WebUI.openBrowser('')
 WebUI.navigateToUrl('https://publicare.ch/')
 
 WebUI.click(findTestObject('Object Repository/productsOR/Page_HOME  Publicare/a_Produkte'))
--- Scripts/Publicare Services/Script1553076854530.groovy
+++ Scripts/Publicare Services/Script1553076854530.groovy
@@ -2,7 +2,6 @@ import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
 import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
 import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
- 
 import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
 import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
 import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
@@ -13,32 +12,8 @@ import com.kms.katalon.core.testobject.TestObject as TestObject
 import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
- 
-// Explicitly set the path of Chrome Driver binary
-System.setProperty("webdriver.chrome.driver", "/var/jenkins_home/tools/chromedriver/chromedriver");
 
-// Explicitly set the path of Chrome Browser binary
-import org.openqa.selenium.chrome.ChromeDriver
-import org.openqa.selenium.chrome.ChromeOptions
-import org.openqa.selenium.remote.DesiredCapabilities
-import com.kms.katalon.core.webui.driver.DriverFactory
-
-optionsBeta = new ChromeOptions();
-//optionsBeta.addArguments("start-maximized"); // open Browser in maximized mode
-//optionsBeta.addArguments("disable-infobars"); // disabling infobars
-//optionsBeta.addArguments("--disable-extensions"); // disabling extensions
-//optionsBeta.addArguments("--disable-gpu"); // applicable to windows os only
-optionsBeta.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-optionsBeta.addArguments("--no-sandbox"); // Bypass OS security model
-optionsBeta.addArguments('--headless');
-optionsBeta.setBinary("/var/jenkins_home/tools/chromedriver/chromedriver");
-DesiredCapabilities capabilities = new DesiredCapabilities();
-capabilities.setCapability(ChromeOptions.CAPABILITY, optionsBeta);
-
-ChromeDriver driver = new ChromeDriver(capabilities);
-DriverFactory.changeWebDriver(driver)
- 
- 
+WebUI.openBrowser('')
 
 WebUI.navigateToUrl('https://publicare.ch/')
 
@@ -53,5 +28,4 @@ WebUI.click(findTestObject('Object Repository/servicesOR/Page_services  Publicar
 WebUI.click(findTestObject('Object Repository/servicesOR/Page_Lieferant  Publicare/a_Dienstleistungen'))
 
 WebUI.closeBrowser()
-
 
