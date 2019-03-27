093b93d8fb1e5dae845bf8126c5d059f9939b6d0
Nasrullah <nasrullah.syed@silbury.com>
2019-03-27 15:38:27 +0530

    added changes from yasar

--- Scripts/Publicare Services/Script1553076854530.groovy
+++ Scripts/Publicare Services/Script1553076854530.groovy
@@ -23,22 +23,23 @@ import org.openqa.selenium.chrome.ChromeOptions
 import org.openqa.selenium.remote.DesiredCapabilities
 import com.kms.katalon.core.webui.driver.DriverFactory
 
-optionsBeta = new ChromeOptions();
+
+ChromeOptions optionsBeta = new ChromeOptions();
 //optionsBeta.addArguments("start-maximized"); // open Browser in maximized mode
 //optionsBeta.addArguments("disable-infobars"); // disabling infobars
 //optionsBeta.addArguments("--disable-extensions"); // disabling extensions
 //optionsBeta.addArguments("--disable-gpu"); // applicable to windows os only
-optionsBeta.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
 optionsBeta.addArguments("--no-sandbox"); // Bypass OS security model
-optionsBeta.addArguments('--headless');
-optionsBeta.setBinary("/usr/bin/google-chrome");
-DesiredCapabilities capabilities = new DesiredCapabilities();
+optionsBeta.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+
+//optionsBeta.addArguments('--headless');
+//optionsBeta.setBinary("/var/jenkins_home/tools/chromedriver/google-chrome");
+/*DesiredCapabilities capabilities = new DesiredCapabilities();
 capabilities.setCapability(ChromeOptions.CAPABILITY, optionsBeta);
 
-ChromeDriver driver = new ChromeDriver(capabilities);
-DriverFactory.changeWebDriver(driver)
- 
- 
+ChromeDriver driver = new ChromeDriver(capabilities);*/
+ChromeDriver driver = new ChromeDriver(optionsBeta);
+//DriverFactory.changeWebDriver(driver)
 
 WebUI.navigateToUrl('https://publicare.ch/')
 
